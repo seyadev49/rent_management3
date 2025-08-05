9f6563c892c370ba88a8349f0f0095b7ec34ae95
seyasmoke12 <122417320+seyasmoke12@users.noreply.github.com>
2025-08-05 21:04:15 +0300

    Deleted Toaster.tsx

--- front-end/src/components/ui/Toaster.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Toaster: React.FC = () => {
-  return (
-    <div id="toast-container" className="fixed top-4 right-4 z-50 space-y-2">
-      {/* Toast notifications will be rendered here */}
-    </div>
-  );
-};
